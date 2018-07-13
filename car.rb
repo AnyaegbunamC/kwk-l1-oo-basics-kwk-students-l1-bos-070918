873cf8bfa0957653b2f2adffec884b8df501f262
Celine Anyaegbunam <cea03@icloud.com>
2018-07-13 19:09:30 +0000

    automatically backed up by learn

--- car.rb
+++ car.rb
@@ -1,32 +1,13 @@
 class Car
+  attr_reader :model , :make , :year ;
+  attr_accessor :heating 
   
   def initialize(model,make,year) 
     @model = model
     @make = make 
     @year = year 
   end 
-  
-def model 
-  @model
-end 
-
-def make 
-  @make 
-end 
-
-def year
-  @year
-end 
-
- def heating=(heating)
-   @heating=heating
- end 
- 
-def heating
-  @heating
-end 
-  
-end 
+  end 
 
 
 
